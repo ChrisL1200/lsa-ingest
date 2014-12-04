578e3c3d27043918364523ad20ecf7b88c847cca
Christopher Lloyd <clloyd1212@gmail.com>
2014-12-03 22:35:29 -0500

    Added directory hashing and not storing photos that already exist

--- photos/ingest.js
+++ photos/ingest.js
@@ -3,18 +3,16 @@ var Home = require('../home/schema');
 var request = require('request');
 var async = require('async');
 var fs = require('graceful-fs');
-var urls = [];
-var lastRunDate = new Date(0);
 var imageRequest = request.defaults({
   encoding: null, pool: {maxSockets: Infinity}
 });
-var started = false;
+
 var homes = 0;
-var sent = 0;
 var received = 0;
 var written = 0;
 var total = 0;
 var timedout = false;
+var finished = false;
 
 exports.ingest = function(callback) {
 	console.log("Beginning photo ingest...");
@@ -29,50 +27,34 @@ exports.ingest = function(callback) {
 			result.photosReceived = 0;
 			_.each(result.listing.photos[0].photo, function(photo){
 				if(photo.mediaurl[0]) {
-					var id = 'xxx/xxx/4xx/yxxx/xxxx'.replace(/[xy]/g, function(c) {
-				    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-				    return v.toString(16);
-					});
-
-					var checkDirectory = id.split('/');
-					_.each([[0],[0,1],[0,1,2],[0,1,2,3]], function(dirs) {
-						var directory = 'images/';
-						_.each(dirs, function(dir, index) {
-							directory += checkDirectory[dir];
-							if(index !== (dirs.length - 1)) {
-								directory += '/';
-							}
+					var directory = 'images/' + (photo.mediaurl[0].hashCode() % 10000).toString();
+					if (!fs.existsSync(directory)){
+					    fs.mkdirSync(directory);
+					}
+					photo.storedId = directory + (photo.mediaurl[0].hashCode().toString()) + '.jpeg';
+						total++;
+					if (!fs.existsSync(directory + (photo.mediaurl[0].hashCode().toString() + '.jpeg'))) {
+						imageRequest({url: photo.mediaurl[0]}, 
+							function (error, response, body) {
+								received++;
+						    if (!error && response.statusCode == 200) {
+					        body = new Buffer(body, 'binary');
+					        fs.writeFile(photo.storedId, body, function (err) {
+					        	if (err) { 
+									  	console.log(err);
+									  }
+					        	photoCallback(result);
+									});
+							  }
+							  else {
+							  	written++;
+							  	console.log(photo.mediaurl[0] + " : " + error);
+							  }
 						});
-						if (!fs.existsSync(directory)){
-						    fs.mkdirSync(directory);
-						}
-					});
-					total++;
-					photo.storedId = id + '.jpeg';
-					imageRequest({url: photo.mediaurl[0]}, 
-						function (error, response, body) {
-							result.photosReceived++;
-							received++;
-					    if (!error && response.statusCode == 200) {
-				        body = new Buffer(body, 'binary');
-				        fs.writeFile('images/' + id + '.jpeg', body, function (err) {
-				        	written++;
-								  if (err) throw err;
-									console.log("received: " + received + " written: " + written + " total: " + total + " homes: " + homes);
-									if(result.photosReceived === result.listing.photos[0].photo.length) {
-										result.save();
-										if(written >= (total - 1)){
-											console.log("DONE!");
-										}
-									}
-								});
-						  }
-						  else {
-						  	written++;
-						  	console.log(photo.mediaurl[0]);
-						  	console.log(response);
-						  }
-					});
+					}
+					else {
+						photoCallback(result);
+					}
 			  }
 		  });
 		}
@@ -80,22 +62,48 @@ exports.ingest = function(callback) {
 	  console.log(err);
 	}).on('close', function () {
 		console.log('Finished');
+		finished = true;
 	});
 }
 
+function photoCallback(result) {
+	result.photosReceived++;
+	written++;
+	console.log("received: " + received + " written: " + written + " total: " + total + " homes: " + homes);
+	if(result.photosReceived === result.listing.photos[0].photo.length) {
+		result.save();
+		if(written >= (total - 1) && finished){
+			console.log("DONE!");
+		}
+	}
+}
+
 function pauseStream(stream) {
-	if((total > (written + 100)) && !timedout) {
+	if((total > (written + 20)) && !timedout) {
 		timedout = true;
 		stream.pause();
 		setTimeout(function() {
 			timedout = false;
 	    pauseStream(stream);
-	  }, 300);
+	  }, 150);
 	}
 	else {
 		stream.resume();
 	}
 }
 
+String.prototype.hashCode = function(){
+	var hash = 0;
+	if (this.length == 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	if(hash < 0) {
+		hash = hash * -1;
+	}
+	return hash;
+}
 // Delete all photos that are expired records
 // Maintain home records
